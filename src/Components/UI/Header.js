adf52c4f885f4baa2ad610c0631252da0d4a37d5
Aniket <aniketupatel1@gmail.com>
2021-07-20 14:38:17 -0400

    added a header with an appbar from material UI

---
+++ src/Components/UI/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+
+const Header =(props)  => {
+
+
+    return (
+        <AppBar>
+            <Toolbar>Arc Dev</Toolbar>
+        </AppBar>
+    )
+}
+
+export default Header
